b1d7d347711874ae237f87f239af93a55c72de39
shivang <shivangmokariya92173@gmail.com>
2022-12-13 17:57:15 +0530

    new Update

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -15,6 +15,7 @@ app.use(cors());
 // controller require
 const userPostData = require("../controllers/userController")
 const getPostData = require("../controllers/userController")
+const getPostDataAll = require("../controllers/userController")
 const userComment=require("../controllers/userController")
 const getComment=require("../controllers/userController") 
 
@@ -23,10 +24,10 @@ const upload = multer({ dest: "uploads/" });
 app.use("/uploads", express.static("uploads"));
 var cpUpload_post = upload.fields([{ name: "profile", maxCount: 1 }, { name: "profilePic", maxCount: 1 }]);
 
-
-
 router.post("/:id",cpUpload_post,middleware,userPostData.userPostData);
 router.get("/getPostData/:id",cpUpload_post,middleware,getPostData.getPostData);
+router.get("/getPostDataAll",middleware,getPostDataAll.getPostDataAll);
+
 router.post("/comment/:id",userComment.userComment);
 router.get("/comment/:id",getComment.getComment);
 
@@ -36,19 +37,14 @@ module.exports = router;
 
 
 
-
-
 // 63844cd1d3b90a46b1ad9798
 
 
 // 638d6c4d188d4f17cab08c8f
-
-
-
-// 95969798
-// Jmb@0825,jmb@8532,Jmb@7736,Hjb@0825,Hjb@8532,Jmb@@130
-// 9909150973
-
-
+// 13/12/2022
+// profile page api add phone no.,
+// signup api same user bug solve,
+// git and github,
+//
 
 // 638d6c7f188d4f17cab08c95
